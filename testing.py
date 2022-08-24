8b5ecba5da7e8a597edaced46e61474e9845aa9d
олександр кац <98089780+itsane4ka@users.noreply.github.com>
2022-08-24 12:39:15 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -71,3 +71,100 @@ class TestLidarSegmentationLabelsValidator(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+""" Testing the Lidar Label Annotation
+"""
+
+import cv2
+import time
+import unittest
+import numpy as np
+from DataValidator import LabelValidator
+from annotation_errors import AnnotationErrors
+from lookup_table_web_of_rail import LABEL_RGB_PALETTE as LABEL_RGB_PALETTE_WEB_OF_RAIL
+from lookup_table_lidar_segmentation import LABEL_RGB_PALETTE as LABEL_RGB_PALETTE_LIDAR
+from lookup_table_simple_for_testing import LABEL_RGB_PALETTE as LABEL_RGB_PALETTE_SIMPLE
+
+
+def generate_picture_1(height, length, color_map):
+    photo = np.zeros((100, 100, 3), dtype="uint8")
+    expected_result = [None, None]
+    pic = cv2.imwrite('test/pic1.png', photo)
+    return [photo, expected_result]
+
+
+def generate_picture_3(height, length, color_map):
+    photo = np.zeros((500, 4000, 3), dtype="uint8")
+    photo[100:102, 300:302] = 185, 14, 24                       # non color map color
+    return photo
+
+
+class TestDataValidator(unittest.TestCase):
+    def setUp(self):
+        # constants
+        self.height_main = 100
+        self.length_main = 100
+        self.amount_of_layers = 3
+        self.folder = "createdTests/"
+        self.image_path_list = []
+        self.images_opencv_list = []
+        self.expected_results_list = []
+        self.SegmentationValidatorList = []
+        self.color_map = LABEL_RGB_PALETTE_SIMPLE
+
+        self.images_opencv_list.append(generate_picture_1(self.height_main, self.length_main, self.color_map)[0])
+        self.expected_results_list.append(generate_picture_1(self.height_main, self.length_main, self.color_map)[1])
+        print(self.images_opencv_list, self.expected_results_list)
+        # self.images_opencv_list.append(generate_picture_2(self.height_main, self.length_main, self.color_map)[0])
+        # self.images_opencv_list.append(generate_picture_3(self.height_main, self.length_main, self.color_map)[0])
+
+"""
+        for image_path, images_opencv in zip(self.image_path_list, self.images_opencv_list):
+            self.SegmentationValidatorList.append(LabelValidator(image_path, images_opencv, self.height_main,
+                                                                 self.length_main, self.amount_of_layers,
+                                                                 LABEL_RGB_PALETTE_WEB_OF_RAIL))
+            
+            
+    def test_check_errors(self):
+        for segmentationValidator, expected in zip(self.SegmentationValidatorList, self.expected_results_list):
+            # additionally check timing
+            start_time = time.time()
+            print(segmentationValidator.check_errors(), "; ", expected)
+            end_time = time.time()
+            print(end_time - start_time)
+
+            self.assertEqual(segmentationValidator.check_errors(), expected)
+"""
+
+if __name__ == "__main__":
+    a = TestDataValidator()
+    a.setUp()
+    #unittest.main()
+
+
+
+
+
+
